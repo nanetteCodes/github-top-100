0ac5b0c88e2208726aa3798b990f969e3b9c233f
Nanette <njulius89@gmail.com>
2020-02-15 22:37:35 -0800

    error handling

--- src/components/Repos/Repos.jsx
+++ src/components/Repos/Repos.jsx
@@ -3,35 +3,39 @@ import RepoItem from '../RepoItem/RepoItem';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 export default class Repos extends Component {
-   state = {
-      repos: [],
-      loading: false,
-   };
-   async componentDidMount() {
-      try {
-         this.setState({ loading: true });
-         const res = await fetch(
-            'https://api.github.com/search/repositories?q=stars&sort=stars&order=desc&per_page=100',
-         );
-         const data = await res.json();
-         if (!res.ok) {
-            throw Error(res.statusText);
-         }
-         this.setState({ repos: data.items, loading: false });
-      } catch (err) {
-         console.log(err);
-      }
-   }
-
-   render() {
-      const { repos, loading } = this.state;
-      return (
-         <>
-            {loading ? <LoadingSpinner /> : null}
-            <div className='container grid'>
-               <RepoItem repos={repos} />
-            </div>
-         </>
+  state = {
+    repos: [],
+    loading: false,
+    errMsg: '',
+  };
+  async componentDidMount() {
+    try {
+      this.setState({ loading: true });
+      const res = await fetch(
+        'https://api.github.com/search/repositostars&sort=stars&order=desc&per_page=100',
       );
-   }
+      const data = await res.json();
+      if (!res.ok) {
+        this.setState({ errMsg: res.statusText, loading: false });
+        throw Error(res.statusText);
+      }
+      this.setState({ repos: data.items, loading: false });
+    } catch (err) {
+      this.setState({ errMsg: err });
+      console.log(err);
+    }
+  }
+
+  render() {
+    const { repos, loading, errMsg } = this.state;
+    return (
+      <>
+        {loading ? <LoadingSpinner /> : null}
+        {errMsg !== '' ? <h1 className='err'>Error</h1> : null}
+        <div className='container grid'>
+          <RepoItem repos={repos} />
+        </div>
+      </>
+    );
+  }
 }
